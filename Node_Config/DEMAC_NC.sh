9f1967afb5c86cf12111a21c5d62e2d99ce46cbd
Darptolus <diegoroa90@gmail.com>
2020-02-29 00:58:22 -0500

    updated files

--- Node_Config/DEMAC_NC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ssh parallella@192.168.10.101 'mkdir DEMAC_config'
-sudo scp ~/CAPSL/DEMAC/DEMAC_set_ip.c parallella@192.168.10.101:~/DEMAC_config/
-ssh parallella@192.168.10.101 'cd ~/DEMAC_config/; g++ -o DEMAC_set_ip.o DEMAC_set_ip.c'
-ssh parallella@192.168.10.101 'cd ~/DEMAC_config/; sudo ./DEMAC_set_ip.o 01'
-ssh parallella@192.168.10.101 'sudo ifdown eth0; ifup eth0'
